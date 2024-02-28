30458a0612fe0b3271f9627d9e726831932a161d
Kseniya <pen.cil@mail.ru>
2024-02-29 00:29:38 +0500

    feat: add button component

---
+++ frontend/components/button.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <button type='button' className='bg-blue text-white w-full py-4 rounded-md'>Показать еще 15 из 20</button>
+  )
+}
+
+export default Button
